79f723827e71eeceb22345771a16f7c78086b847
the-bears-field <thebearsfield@gmail.com>
2021-09-13 00:18:13 +0900

    Postモデル生成、Userモデル修正。 両方とも多対多のリレーションを定義。 Illuminate\Database\Eloquent\Relations\BelongsToManyインポート。 リレーション目的のメソッドを追加。
    参考:
    https://readouble.com/laravel/8.x/ja/eloquent-relationships.html#many-to-many
    https://laravel.com/api/8.x/Illuminate/Database/Eloquent/Concerns/HasRelationships.html#method_belongsToMany

---
+++ myapp/app/Models/Post.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    /**
+     * The users that belong to the Post
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'post_user');
+    }
+}
--- myapp/app/Models/User.php
+++ myapp/app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class User extends Authenticatable
 {
@@ -40,4 +41,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * The posts that belong to the User
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function posts(): BelongsToMany
+    {
+        return $this->belongsToMany(Post::class, 'post_user');
+    }
 }
