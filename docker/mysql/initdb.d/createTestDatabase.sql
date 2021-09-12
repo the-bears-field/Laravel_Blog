91b9de1c15ab3551f3a28d2f8e61e41bd11ce9cd
the-bears-field <thebearsfield@gmail.com>
2021-09-12 15:29:34 +0900

    Initial commit.

---
+++ docker/mysql/initdb.d/createTestDatabase.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS laravel_test;
+CREATE DATABASE laravel_test DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+GRANT ALL PRIVILEGES ON `laravel\_test`.* TO 'user'@'%';
---
+++ initializeAppContainer.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd myapp
+cp .env.example .env
+composer install
+npm install
+php artisan key:generate
