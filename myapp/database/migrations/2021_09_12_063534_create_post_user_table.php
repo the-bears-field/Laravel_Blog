7a30e630c6fe5e9ea3c95c91496a73e8dffa5e13
the-bears-field <thebearsfield@gmail.com>
2021-09-12 23:14:09 +0900

    migrationファイルを生成。 postsテーブルと、postsテーブルとusersテーブルの交差テーブルであるpost_userテーブルを作成。 交差テーブルのuser_id、post_idカラムに外部キー制約とUNIQUE制約を付与。
    参考:
    https://readouble.com/laravel/8.x/ja/eloquent-relationships.html#many-to-many
    SQLアンチパターン 3章 IDリクワイアド(とりあえずID)

---
+++ myapp/database/migrations/2021_09_12_063534_create_post_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_user', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned();
+            $table->bigInteger('post_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('post_id')->references('id')->on('posts');
+            $table->unique(['user_id', 'post_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_user');
+    }
+}
