0928fd36897e7730b190a1835cce9f9488414a38
the-bears-field <thebearsfield@gmail.com>
2021-09-15 13:59:14 +0900

    Postコントローラ編集。 indexアクションを追加。

--- myapp/app/Http/Controllers/PostController.php
+++ myapp/app/Http/Controllers/PostController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $posts = Post::all();
+        return view('post.index', ['posts' => $posts]);
+    }
 }
