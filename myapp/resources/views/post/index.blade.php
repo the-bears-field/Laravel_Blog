b588d9bc571ac4c036549e194b41abc6672d0f49
the-bears-field <thebearsfield@gmail.com>
2021-10-13 14:42:23 +0900

    index.blade.php編集。 投稿日、更新日、タグ一覧を出力するよう修正。

--- myapp/resources/views/post/index.blade.php
+++ myapp/resources/views/post/index.blade.php
@@ -6,6 +6,15 @@
     @foreach($posts as $post)
         <div>
             <p><a href="{{ route('post.show', ['postId' => $post->id]) }}">{{ $post->title }}</a></p>
+            <time datetime="{{ $post->created_at->toDateString() }}">{{ $post->created_at->format('Y年n月j日') }}</time>
+            @if($post->created_at->toDateTimeString() !== $post->updated_at->toDateTimeString())
+                <time datetime="{{ $post->updated_at->toDateString() }}">{{ $post->updated_at->format('Y年n月j日') }}</time>
+            @endif
+            <ul>
+                @foreach($post->tags as $tag)
+                    <li><a href="null">{{ $tag->name }}</a></li>
+                @endforeach
+            </ul>
             <p>{{ $post->post }}</p>
         </div>
     @endforeach
