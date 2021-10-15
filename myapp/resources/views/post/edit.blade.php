058fd7d947191df42c1fcd4ef4a8c1d2e12d3370
the-bears-field <thebearsfield@gmail.com>
2021-10-16 01:47:05 +0900

    editアクションに対応するビューの表示を変更。 tagを入力するテキストボックスを追加。

--- myapp/resources/views/post/edit.blade.php
+++ myapp/resources/views/post/edit.blade.php
@@ -2,5 +2,6 @@
     @csrf
     <div><input type="text" name="title" value="{{ $title }}"></div>
     <textarea name="post">{{ $post }}</textarea>
+    <div><input type="text" name="tags" value="{{ $tags }}"></div>
     <div><input type="submit" value="投稿"></div>
 </form>
