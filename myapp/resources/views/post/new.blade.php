24fdc3e6fbd5c6b2eec32275a9e4c2ce0b53f348
the-bears-field <thebearsfield@gmail.com>
2021-10-15 14:58:46 +0900

    newアクションに対応するビューの表示を変更。 フォームにタグを入力するテキストボックスを追加。

--- myapp/resources/views/post/new.blade.php
+++ myapp/resources/views/post/new.blade.php
@@ -2,5 +2,6 @@
     @csrf
     <div><input type="text" name="title"></div>
     <textarea name="post"></textarea>
+    <div><input type="text" name="tags"></div>
     <div><input type="submit" value="投稿"></div>
 </form>
