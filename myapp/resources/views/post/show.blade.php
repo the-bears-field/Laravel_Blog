3e4f1aaacf28b71cc45550e1c4976d38bb5b29eb
the-bears-field <thebearsfield@gmail.com>
2021-10-15 14:56:50 +0900

    showアクションに対応するビューの表示を変更。 投稿日、更新日、タグ一覧を出力するよう修正。

--- myapp/resources/views/post/show.blade.php
+++ myapp/resources/views/post/show.blade.php
@@ -1,4 +1,13 @@
 <div>
     <p>{{ $post->title }}</p>
+    <time datetime="{{ $post->created_at->toDateString() }}">{{ $post->created_at->format('Y年n月j日') }}</time>
+    @if($post->created_at->toDateTimeString() !== $post->updated_at->toDateTimeString())
+        <time datetime="{{ $post->updated_at->toDateString() }}">{{ $post->updated_at->format('Y年n月j日') }}</time>
+    @endif
+    <ul>
+    @foreach($post->tags as $tag)
+        <li>{{ $tag }}</li>
+    @endforeach
+    </ul>
     <p>{{ $post->post }}</p>
 </div>
