0993e7dd83b8cc6ed877aaa355267601aa23185f
the-bears-field <thebearsfield@gmail.com>
2021-10-22 15:13:38 +0900

    PostService編集。 PostRepositoryのインポート処理をPostRepositoryInterfaceに変更。 UserRepositoryInterface、TagRepositoryInterface、Authインポート。 tagRepositoryプロパティ、userRepositoryプロパティ追加。 postRepositoryプロパティの型を変更。 コンストラクタの引数を追加、 userRepositoryプロパティ、 postRepositoryプロパティにそれぞれのリポジトリインスタンスを代入する処理を追加。

--- myapp/app/Services/PostService.php
+++ myapp/app/Services/PostService.php
@@ -3,16 +3,27 @@ declare(strict_types=1);
 
 namespace App\Services;
 
-use App\Repositories\PostRepository;
+use App\Repositories\PostRepositoryInterface;
+use App\Repositories\UserRepositoryInterface;
+use App\Repositories\TagRepositoryInterface;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Auth;
 
 class PostService implements PostServiceInterface
 {
-    private PostRepository $postRepository;
+    private PostRepositoryInterface $postRepository;
+    private TagRepositoryInterface  $tagRepository;
+    private UserRepositoryInterface $userRepository;
 
-    public function __construct(PostRepository $postRepository)
+    public function __construct(
+        PostRepositoryInterface $postRepository,
+        TagRepositoryInterface  $tagRepository,
+        UserRepositoryInterface $userRepository
+    )
     {
         $this->postRepository = $postRepository;
+        $this->tagRepository  = $tagRepository;
+        $this->userRepository = $userRepository;
     }
 
     public function getAll(): Collection
