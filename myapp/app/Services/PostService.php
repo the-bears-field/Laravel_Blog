c2ba9e56e0189a179b36c9cf5817c81b642725dc
the-bears-field <thebearsfield@gmail.com>
2021-10-22 15:19:11 +0900

    PostServiceにcreatePostメソッド、stringToArrayメソッド追加。 createPostメソッド中にpostsテーブル、tagsテーブルへの挿入処理、中間テーブルとの紐付け処理を記述。
    PostServiceTestにcreatePostメソッドのテストを追加し、正常に作動することを確認。

--- myapp/app/Services/PostService.php
+++ myapp/app/Services/PostService.php
@@ -35,4 +35,49 @@ class PostService implements PostServiceInterface
     {
         return $this->postRepository->getPost($postId);
     }
+
+    public function createPost($request): void
+    {
+        $post = $this->postRepository->createPost($request);
+        $user = Auth::user();
+        $user->posts()->syncWithoutDetaching(intval($post->id));
+
+        $sentTagNames      = $this->stringToArray($request->tags);
+        $availableTagNames = $this->tagRepository->getAvailableTagNames();
+
+        foreach($sentTagNames as $tagName){
+            $isExistTag =  in_array($tagName, $availableTagNames, true);
+
+            if(!$isExistTag) {
+                $insertedTag        = $this->tagRepository->createTag($tagName);
+                $availableTagNames += [$insertedTag->id => $insertedTag->name];
+            }
+
+            $attachTagIds[] = array_search($tagName, $availableTagNames, true);
+        }
+
+        $post->tags()->syncWithoutDetaching($attachTagIds);
+    }
+
+    /**
+     * スペースで区切った文字列を配列に変換。
+     *
+     * @return array
+     */
+    private function stringToArray (string $string): array
+    {
+        // 全角スペースを半角へ
+        $string = preg_replace('/(\xE3\x80\x80)/', ' ', $string);
+        // 両サイドのスペースを消す
+        $string = trim($string);
+        // 改行、タブをスペースに変換
+        $string = preg_replace('/[\n\r\t]/', ' ', $string);
+        // 複数スペースを一つのスペースに変換
+        $string = preg_replace('/\s{2,}/', ' ', $string);
+        //文字列を配列に変換
+        $array = preg_split('/[\s]/', $string, -1, PREG_SPLIT_NO_EMPTY);
+        $array = array_unique($array);
+        $array = array_values($array);
+        return $array;
+    }
 }
--- myapp/tests/Feature/PostServiceTest.php
+++ myapp/tests/Feature/PostServiceTest.php
@@ -2,10 +2,11 @@
 
 namespace Tests\Feature;
 
+use App\Http\Requests\PostRequest;
 use App\Models\Post;
 use App\Models\Tag;
 use App\Models\User;
-use App\Services\PostService;
+use App\Services\PostServiceInterface;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\App;
@@ -51,15 +52,44 @@ class PostServiceTest extends TestCase
      */
     public function test_getAllメソッドで記事一覧が取得可能()
     {
-        $postService = App::make(PostService::class);
+        $postService = App::make(PostServiceInterface::class);
         $post = $postService->getAll();
         $this->assertTrue($post->isNotEmpty());
     }
 
     public function test_getPostメソッドでPostインスタンスが取得可能()
     {
-        $postService = App::make(PostService::class);
+        $postService = App::make(PostServiceInterface::class);
         $post = $postService->getPost(1);
         $this->assertEquals($post::class, Post::class);
+    }
+
+    public function test_createPostメソッドが正常に作動する()
+    {
+        $this->actingAs($this->user);
+
+        $request = new PostRequest;
+        $params  = [
+            'title' => 'example',
+            'post'  => 'example',
+            'tags'  => 'example1 example2 example3',
+        ];
+        $request->merge($params);
+
+        $postService = App::make(PostServiceInterface::class);
+        $postService->createPost($request);
+
+        $this->assertDatabaseHas('posts', [
+            'title' => 'example',
+            'post'  => 'example'
+        ]);
+
+        $verificationTagNames = ['example1', 'example2', 'example3'];
+
+        foreach($verificationTagNames as $tagName){
+            $this->assertDatabaseHas('tags', [
+                'name' => $tagName
+            ]);
+        }
     }
 }
