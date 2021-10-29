76061d2aecf39d897c52efe121d61ca4033d5ccf
the-bears-field <thebearsfield@gmail.com>
2021-10-29 13:58:54 +0900

    シーダファイル編集。 ファイル名変更(PostUserSeeder.php -> PostTagUserSeeder.php) シーディング時にユーザ、投稿、タグとの紐付けの処理方法を変更。
    上記変更に伴い、DatabaseSeederのrunメソッド中のcallメソッドの引数を変更。
    
    参考:
    https://readouble.com/laravel/8.x/ja/collections.html#method-each

---
+++ myapp/database/seeders/PostTagUserSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\Post;
+use App\Models\Tag;
+use App\Models\User;
+
+class PostTagUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $params = [
+            'name' => 'test',
+            'email' => 'test@example.com',
+            'password' => Hash::make('123456')
+        ];
+        $user  = User::factory()->create($params);
+        $tags  = Tag::factory()->count(10)->create();
+        $posts = Post::factory()
+                        ->count(10)
+                        ->create()
+                        ->each(function($post) use ($user, $tags) {
+                            $post->users()->syncWithoutDetaching($user->id);
+                            $post->tags()->syncWithoutDetaching(
+                                $tags->random(random_int(1, 10))->pluck('id')->toArray()
+                            );
+                        });
+    }
+}
