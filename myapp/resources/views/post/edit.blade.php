40c755df905a6cdfac1ed0930a4783e2d6ed744b
the-bears-field <thebearsfield@gmail.com>
2021-10-31 17:47:34 +0900

    editアクションに対応するビューファイル編集。 フォームタグの属性のタイポミス修正。 Postインスタンスに含まれるレコードの出力方法を変更。

--- myapp/resources/views/post/edit.blade.php
+++ myapp/resources/views/post/edit.blade.php
@@ -1,7 +1,7 @@
-<form adtion="/new" method="POST">
+<form action="/edit" method="POST">
     @csrf
-    <div><input type="text" name="title" value="{{ $title }}"></div>
-    <textarea name="post">{{ $post }}</textarea>
-    <div><input type="text" name="tags" value="{{ $tags }}"></div>
+    <div><input type="text" name="title" value="{{ $post->title }}"></div>
+    <textarea name="post">{{ $post->post }}</textarea>
+    <div><input type="text" name="tags" value="{{ $post->tags }}"></div>
     <div><input type="submit" value="投稿"></div>
 </form>
