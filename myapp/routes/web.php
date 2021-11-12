c38ce1feb7b035367a4bc5678088a2e67a3a3599
the-bears-field <thebearsfield@gmail.com>
2021-11-12 11:07:47 +0900

    ルーティング情報編集。 TagControllerインポート。 "/tag/{tagName}"へのルーティングを追加。

--- myapp/routes/web.php
+++ myapp/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\TagController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\HomeController;
 
@@ -33,3 +34,4 @@ Route::middleware(['auth'])->group(function () {
 });
 
 Route::get('/{postId}', [PostController::class, 'show'])->name('post.show');
+Route::get('/tag/{tagName}', [TagController::class, 'index'])->name('tag.index');
