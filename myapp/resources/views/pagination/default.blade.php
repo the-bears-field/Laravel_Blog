a398b4735da24b861ca28656933c9255158bcf55
the-bears-field <thebearsfield+github@gmail.com>
2021-11-21 08:10:54 +0900

    ページネーションのビューを編集。 デフォルト設定をベースに構造を変更。

--- myapp/resources/views/pagination/default.blade.php
+++ myapp/resources/views/pagination/default.blade.php
@@ -1,17 +1,34 @@
-@if($paginator->lastPage() > 1)
+@if($paginator->hasPages())
+    {{-- Previous Page Link --}}
     <div class="paginator__nav flex-direction-row">
-        @if($paginator->currentPage() !== 1)
-            <a href="/" class="pagenator__link" title="previous page"><i class="fas fa-chevron-left"></i></a>
+        @if(!$paginator->onFirstPage())
+            <a href="{{ $paginator->url(1) }}" class="pagenator__link" title="first page"><i class="fas fa-angle-double-left"></i></a>
+            <a href="{{ $paginator->previousPageUrl() }}" class="pagenator__link" title="previous page"><i class="fas fa-chevron-left"></i></a>
         @endif
-        @for($i = 1; $i <= $paginator->lastPage(); $i++)
-            @if($paginator->currentPage() === $i)
-                <span class="pagenator__link_current"><b>{{ $i }}</b></span>
-            @else
-                <a href="{{ $i === 1 ? '/' : '/?page='. $i }}" class="pagenator__link" title="page {{ $i }}">{{ $i }}</a>
+
+        {{-- Pagination Elements --}}
+        @foreach ($elements as $element)
+            {{-- "Three Dots" Separator --}}
+            @if (is_string($element))
+                <span class="pagenator__link" aria-disabled="true"><i class="fas fa-ellipsis-h"></i></span>
             @endif
-        @endfor
-        @if($paginator->currentPage() !== $paginator->lastPage())
-            <a href="/?page={{ $paginator->lastPage() }}" class="pagenator__link" title="next page"><i class="fas fa-chevron-right"></i></a>
+
+            {{-- Array Of Links --}}
+            @if (is_array($element))
+                @foreach ($element as $page => $url)
+                    @if ($page == $paginator->currentPage())
+                       <span class="pagenator__link_current"><b>{{ $page }}</b></span>
+                    @else
+                        <a href="{{ $url }}" class="pagenator__link" title="page {{ $page }}">{{ $page }}</a>
+                    @endif
+                @endforeach
+            @endif
+        @endforeach
+
+        {{-- Next Page Link --}}
+        @if($paginator->hasMorePages())
+            <a href="{{ $paginator->nextPageUrl() }}" class="pagenator__link" title="next page"><i class="fas fa-chevron-right"></i></a>
+            <a href="{{ $paginator->url($paginator->lastPage()) }}" class="pagenator__link" title="last page"><i class="fas fa-angle-double-right"></i></a>
         @endif
     </div>
 @endif
