e6945b3c5d3530ac87915f4efb3aa7509a2f31f2
the-bears-field <thebearsfield+github@gmail.com>
2021-11-24 23:20:41 +0900

    Userリポジトリ編集。 Illuminate\Support\Facades\Authインポート。 updateUserメソッド追加。

--- myapp/app/Repositories/UserRepository.php
+++ myapp/app/Repositories/UserRepository.php
@@ -4,8 +4,13 @@ declare(strict_types=1);
 namespace App\Repositories;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class UserRepository implements UserRepositoryInterface
 {
-    //
+    public function updateUser($request): void
+    {
+        $params = $request->only('name', 'email', 'password');
+        Auth::user()->update($params);
+    }
 }
--- myapp/app/Repositories/UserRepositoryInterface.php
+++ myapp/app/Repositories/UserRepositoryInterface.php
@@ -7,5 +7,5 @@ use App\Models\User;
 
 interface UserRepositoryInterface
 {
-    //
+    public function updateUser($request): void;
 }
