a27bddb2af558d4e8fdf212a86fdff5893a939c7
the-bears-field <thebearsfield+github@gmail.com>
2021-12-02 14:32:34 +0900

    Userサービスプロバイダ編集。 Postリポジトリ、Tagリポジトリ関連をインポート。 サービスコンテナへUserサービスを登録する際の引数を追加。

--- myapp/app/Providers/UserServiceProvider.php
+++ myapp/app/Providers/UserServiceProvider.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Repositories\PostRepository;
+use App\Repositories\PostRepositoryInterface;
+use App\Repositories\TagRepository;
+use App\Repositories\TagRepositoryInterface;
 use App\Repositories\UserRepository;
 use App\Repositories\UserRepositoryInterface;
 use App\Services\UserService;
@@ -25,6 +29,8 @@ class UserServiceProvider extends ServiceProvider
 
         $this->app->bind(UserServiceInterface::class, function ($app): UserService {
                 return new UserService(
+                    $app->make(PostRepositoryInterface::class),
+                    $app->make(TagRepositoryInterface::class),
                     $app->make(UserRepositoryInterface::class)
                 );
             }
